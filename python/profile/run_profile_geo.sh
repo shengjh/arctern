fc9250e3098c73d98ca00e0deb4cc65308330acb
shengjh <1572099106@qq.com>
2020-03-28 19:25:07 +0800

    remove useless scripe

--- python/profile/run_profile_geo.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin sh
-cd /home/shengjh/work/arctern/python/profile/
-for i in {0..36} ; do
-  # shellcheck disable=SC1068
-  py-spy record --native --output ${i} -- python profile_geo.py -f ${i}
-done
