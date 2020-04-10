5e8c7b4611e33a96f18195a110c76bb98097b46b
shengjh <1572099106@qq.com>
2020-04-10 11:17:09 +0800

    add gen data for test

--- spark/pyspark/profile/gis/gen_data.py
+++ spark/pyspark/profile/gis/gen_data.py
@@ -42,127 +42,15 @@ import pandas as pd
 from hdfs import InsecureClient
 
 
-def gen_st_point():
-    total = rows
-    with client_hdfs.write(os.path.join(data_path, 'st_points.csv'), overwrite=True, encoding='utf-8') as writer:
-        while True:
-            x = [0.1] * row_per_batch
-            y = [0.2] * row_per_batch
-            df = pd.DataFrame(data={'x': x, 'y': y})
-            if total == rows:
-                df.to_csv(writer, index=False)
-            else:
-                df.to_csv(writer, index=False, header=False)
-            total -= row_per_batch
-            if total <= 0:
-                break
-
-
-def gen_st_intersection():
-    total = rows
-    with client_hdfs.write(os.path.join(data_path, 'st_intersection.csv'), overwrite=True, encoding='utf-8') as writer:
-        while True:
-            left = ['POINT(0 0)'] * row_per_batch
-            right = ['LINESTRING ( 2 0, 0 2 )'] * row_per_batch
-            df = pd.DataFrame(data={'left': left, 'right': right})
-            if total == rows:
-                df.to_csv(writer, index=False)
-            else:
-                df.to_csv(writer, index=False, header=False)
-            total -= row_per_batch
-            if total <= 0:
-                break
-
-
-def gen_st_isvalid():
-    total = rows
-    with client_hdfs.write(os.path.join(data_path, 'st_isvalid.csv'), overwrite=True, encoding='utf-8') as writer:
-        while True:
-            geos = ['POINT (30 10)'] * row_per_batch
-            df = pd.DataFrame(data={'geos': geos})
-            if total == rows:
-                df.to_csv(writer, index=False)
-            else:
-                df.to_csv(writer, index=False, header=False)
-            total -= row_per_batch
-            if total <= 0:
-                break
-
-
-def gen_st_equals():
-    total = rows
-    with client_hdfs.write(os.path.join(data_path, 'st_equals.csv'), overwrite=True, encoding='utf-8') as writer:
-        while True:
-            left = ['LINESTRING(0 0, 10 10)'] * row_per_batch
-            right = ['LINESTRING(0 0, 5 5, 10 10)'] * row_per_batch
-            df = pd.DataFrame(data={'left': left, 'right': right})
-            if total == rows:
-                df.to_csv(writer, index=False)
-            else:
-                df.to_csv(writer, index=False, header=False)
-            total -= row_per_batch
-            if total <= 0:
-                break
-
-
-def gen_st_touches():
-    total = rows
-    with client_hdfs.write(os.path.join(data_path, 'st_touches.csv'), overwrite=True, encoding='utf-8') as writer:
-        while True:
-            left = ['LINESTRING(0 0, 1 1, 0 2)'] * row_per_batch
-            right = ['POINT(1 1)'] * row_per_batch
-            df = pd.DataFrame(data={'left': left, 'right': right})
-            if total == rows:
-                df.to_csv(writer, index=False)
-            else:
-                df.to_csv(writer, index=False, header=False)
-            total -= row_per_batch
-            if total <= 0:
-                break
-
-
-def gen_st_overlaps():
-    total = rows
-    with client_hdfs.write(os.path.join(data_path, 'st_overlaps.csv'), overwrite=True, encoding='utf-8') as writer:
-        while True:
-            left = ['POLYGON((1 1, 4 1, 4 5, 1 5, 1 1))'] * row_per_batch
-            right = ['POLYGON((3 2, 6 2, 6 6, 3 6, 3 2))'] * row_per_batch
-            df = pd.DataFrame(data={'left': left, 'right': right})
-            if total == rows:
-                df.to_csv(writer, index=False)
-            else:
-                df.to_csv(writer, index=False, header=False)
-            total -= row_per_batch
-            if total <= 0:
-                break
-
-
-def gen_st_crosses():
-    total = rows
-    with client_hdfs.write(os.path.join(data_path, 'st_crosses.csv'), overwrite=True, encoding='utf-8') as writer:
-        while True:
-            left = ['MULTIPOINT((1 3), (4 1), (4 3))'] * row_per_batch
-            right = ['POLYGON((2 2, 5 2, 5 5, 2 5, 2 2))'] * row_per_batch
-            df = pd.DataFrame(data={'left': left, 'right': right})
-            if total == rows:
-                df.to_csv(writer, index=False)
-            else:
-                df.to_csv(writer, index=False, header=False)
-            total -= row_per_batch
-            if total <= 0:
-                break
-
-
 class _OneColDecorator(object):
     def __init__(self, f, line):
-        self._function_name = f.__name__
         self._line = line
-        self._hdfs_file = os.path.join(data_path, f.__name__ + '.csv')
+        self._file_name = f.__name__[4:] + '.csv'
 
     def __call__(self):
-        print(rows)
         total = rows
-        with client_hdfs.write(self._hdfs_file, overwrite=True, encoding='utf-8') as writer:
+        hdfs_file = os.path.join(data_path, self._file_name)
+        with client_hdfs.write(hdfs_file, overwrite=True, encoding='utf-8') as writer:
             while True:
                 geos = [self._line] * row_per_batch
                 df = pd.DataFrame(data={'geos': geos})
@@ -185,6 +73,74 @@ def OneColDecorator(f=None, line=''):
         return wrapper
 
 
+class _TwoColDecorator(object):
+    def __init__(self, f, left, right):
+        self._left = left
+        self._right = right
+        self._file_name = f.__name__[4:] + '.csv'
+
+    def __call__(self):
+        total = rows
+        hdfs_file = os.path.join(data_path, self._file_name)
+        with client_hdfs.write(hdfs_file, overwrite=True, encoding='utf-8') as writer:
+            while True:
+                left = [self._left] * row_per_batch
+                right = [self._right] * row_per_batch
+                df = pd.DataFrame(data={'left': left, 'right': right})
+                if total == rows:
+                    df.to_csv(writer, index=False)
+                else:
+                    df.to_csv(writer, index=False, header=False)
+                total -= row_per_batch
+                if total <= 0:
+                    break
+
+
+def TwoColDecorator(f=None, left='', right=''):
+    if f:
+        return _TwoColDecorator(f)
+    else:
+        def wrapper(f):
+            return _TwoColDecorator(f, left, right)
+
+        return wrapper
+
+
+@TwoColDecorator(left=0.1, right=0.2)
+def gen_st_point():
+    pass
+
+
+@TwoColDecorator(left='POINT(0 0)', right='LINESTRING ( 2 0, 0 2 )')
+def gen_st_intersection():
+    pass
+
+
+@OneColDecorator(line='POINT (30 10)')
+def gen_st_isvalid():
+    pass
+
+
+@TwoColDecorator(left='LINESTRING(0 0, 10 10)', right='LINESTRING(0 0, 5 5, 10 10)')
+def gen_st_equals():
+    pass
+
+
+@TwoColDecorator(left='LINESTRING(0 0, 1 1, 0 2)', right='POINT(1 1)')
+def gen_st_touches():
+    pass
+
+
+@TwoColDecorator(left='POLYGON((1 1, 4 1, 4 5, 1 5, 1 1))', right='POLYGON((3 2, 6 2, 6 6, 3 6, 3 2))')
+def gen_st_overlaps():
+    pass
+
+
+@TwoColDecorator(left='MULTIPOINT((1 3), (4 1), (4 3))', right='POLYGON((2 2, 5 2, 5 5, 2 5, 2 2))')
+def gen_st_crosses():
+    pass
+
+
 @OneColDecorator(line='POLYGON((1 2, 3 4, 5 6, 1 2))')
 def gen_st_issimple():
     pass
@@ -192,6 +148,134 @@ def gen_st_issimple():
 
 @OneColDecorator(line='LINESTRING(77.29 29.07,77.42 29.26,77.27 29.31,77.29 29.07)')
 def gen_st_geometry_type():
+    pass
+
+
+@OneColDecorator(line='LINESTRING(0 0, 10 0, 20 0, 20 0, 30 0)')
+def gen_st_make_valid():
+    pass
+
+
+@OneColDecorator(
+    line='POLYGON((8 25, 28 22, 28 20, 15 11, 33 3, 56 30, 46 33, 46 34, 47 44, 35 36, 45 33, 43 19, 29 21, 29 22, 35 26, 24 39, 8 25))')
+def gen_st_simplify_preserve_topology():
+    pass
+
+
+@
+def gen_st_polygon_from_envelope():
+    pass
+
+
+@TwoColDecorator(
+    left='POLYGON((-1 3,2 1,0 -3,-1 3))', right='POLYGON((0 2,1 1,0 -1,0 2))'
+)
+def gen_st_contains():
+    pass
+
+
+@TwoColDecorator(
+    left='POINT(0 0)', right='LINESTRING ( 0 0, 0 2 )'
+)
+def gen_st_intersects():
+    pass
+
+
+@TwoColDecorator(
+    left='POLYGON((2 2, 7 2, 7 5, 2 5, 2 2))', right='POLYGON((1 1, 8 1, 8 7, 1 7, 1 1))'
+)
+def gen_st_within():
+    pass
+
+
+@TwoColDecorator(
+    left='POLYGON((-1 -1,2 2,0 1,-1 -1))', right='POLYGON((5 2,7 4,5 5,5 2))'
+)
+def gen_st_distance():
+    pass
+
+
+@TwoColDecorator(
+    left='POLYGON((10 20,10 30,20 30,30 10))', right='POLYGON((10 20,10 40,30 40,40 10))'
+)
+def gen_st_area():
+    pass
+
+
+@OneColDecorator(line='MULTIPOINT ( -1 0, -1 2, -1 3, -1 4, -1 7, 0 1, 0 3, 1 1, 2 0, 6 0, 7 8, 9 8, 10 6 )')
+def gen_st_centroid():
+    pass
+
+
+@OneColDecorator(line='LINESTRING(-72.1260 42.45, -72.1240 42.45666, -72.123 42.1546)')
+def gen_st_length():
+    pass
+
+
+@TwoColDecorator(
+    left='POLYGON((0 0 ,0 1, 1 1, 1 0, 0 0))', right='POLYGON((0 0 ,0 2, 1 1, 1 0, 0 0))'
+)
+def gen_st_hausdorffdistance():
+    pass
+
+@OneColDecorator(line='GEOMETRYCOLLECTION(LINESTRING(2.5 3,-2 1.5), POLYGON((0 1,1 3,1 -2,0 1)))')
+def gen_st_convexhull():
+    pass
+
+@OneColDecorator(line='LINESTRING(77.29 29.07,77.42 29.26,77.27 29.31,77.29 29.07)')
+def gen_st_npoints():
+    pass
+
+@OneColDecorator(line='multipolygon (((0 0, 10 0, 10 10, 0 10, 0 0), (11 11, 20 11, 20 20, 20 11, 11 11)))')
+def gen_st_envelope():
+    pass
+
+@OneColDecorator(line='POLYGON((0 0,1 0,1 1,0 0))')
+def gen_st_buffer():
+    pass
+
+@OneColDecorator(line='POLYGON ((1 1,1 2,2 2,2 1,1 1))')
+def gen_st_union_aggr():
+    pass
+
+@OneColDecorator(line='POLYGON ((0 0,4 0,4 4,0 4,0 0))')
+def gen_st_envelope_aggr():
+    pass
+
+@OneColDecorator(line='POINT (10 10)')
+def gen_st_transform():
+    pass
+
+@OneColDecorator(line='CURVEPOLYGON(CIRCULARSTRING(0 0, 4 0, 4 4, 0 4, 0 0))')
+def gen_st_curvetoline():
+    pass
+
+@OneColDecorator(line="{\"type\":\"Polygon\",\"coordinates\":[[[0,0],[0,1],[1,1],[1,0],[0,0]]]}")
+def gen_st_geomfromgeojson():
+    pass
+
+@OneColDecorator(line='POINT (30 10)')
+def gen_st_pointfromtext():
+    pass
+
+@OneColDecorator(line='POLYGON ((0 0, 0 1, 1 1, 1 0, 0 0))')
+def gen_st_polygonfromtext():
+    pass
+
+@OneColDecorator(line='LINESTRING (0 0, 0 1, 1 1, 1 0)')
+def gen_st_linestringfromtext():
+    pass
+
+@OneColDecorator(line='POLYGON ((0 0, 0 1, 1 1, 1 0, 0 0))')
+def gen_st_geomfromwkt():
+    pass
+
+@OneColDecorator(line='POLYGON ((0 0, 0 1, 1 1, 1 0, 0 0))')
+def gen_st_geomfromtext():
+    pass
+
+@OneColDecorator(line='POLYGON ((0 0, 0 1, 1 1, 1 0, 0 0))')
+def gen_st_astext():
     pass
 
 
@@ -205,32 +289,32 @@ funcs = {
     'st_crosses': gen_st_crosses,
     'st_issimple': gen_st_issimple,
     'st_geometry_type': gen_st_geometry_type,
-    # 'st_make_valid': gen_st_make_valid,
-    # 'st_simplify_preserve_topology': gen_st_simplify_preserve_topology,
-    # 'st_polygon_from_envelope': gen_st_polygon_from_envelope,
-    # 'st_contains': gen_st_contains,
-    # 'st_intersects': gen_st_intersects,
-    # 'st_within': gen_st_within,
-    # 'st_distance': gen_st_distance,
-    # 'st_area': gen_st_area,
-    # 'st_centroid': gen_st_centroid,
-    # 'st_length': gen_st_length,
-    # 'st_hausdorffdistance': gen_st_hausdorffdistance,
-    # 'st_convexhull': gen_st_convexhull,
-    # 'st_npoints': gen_st_npoints,
-    # 'st_envelope': gen_st_envelope,
-    # 'st_buffer': gen_st_buffer,
-    # 'st_union_aggr': gen_st_union_aggr,
-    # 'st_envelope_aggr': gen_st_envelope_aggr,
-    # 'st_transform': gen_st_transform,
-    # 'st_curvetoline': gen_st_curvetoline,
-    # 'st_geomfromgeojson': gen_st_geomfromgeojson,
-    # 'st_pointfromtext': gen_st_pointfromtext,
-    # 'st_polygonfromtext': gen_st_polygonfromtext,
-    # 'st_linestringfromtext': gen_st_linestringfromtext,
-    # 'st_geomfromwkt': gen_st_geomfromwkt,
-    # 'st_geomfromtext': gen_st_geomfromtext,
-    # 'st_astext': gen_st_astext,
+    'st_make_valid': gen_st_make_valid,
+    'st_simplify_preserve_topology': gen_st_simplify_preserve_topology,
+    'st_polygon_from_envelope': gen_st_polygon_from_envelope,
+    'st_contains': gen_st_contains,
+    'st_intersects': gen_st_intersects,
+    'st_within': gen_st_within,
+    'st_distance': gen_st_distance,
+    'st_area': gen_st_area,
+    'st_centroid': gen_st_centroid,
+    'st_length': gen_st_length,
+    'st_hausdorffdistance': gen_st_hausdorffdistance,
+    'st_convexhull': gen_st_convexhull,
+    'st_npoints': gen_st_npoints,
+    'st_envelope': gen_st_envelope,
+    'st_buffer': gen_st_buffer,
+    'st_union_aggr': gen_st_union_aggr,
+    'st_envelope_aggr': gen_st_envelope_aggr,
+    'st_transform': gen_st_transform,
+    'st_curvetoline': gen_st_curvetoline,
+    'st_geomfromgeojson': gen_st_geomfromgeojson,
+    'st_pointfromtext': gen_st_pointfromtext,
+    'st_polygonfromtext': gen_st_polygonfromtext,
+    'st_linestringfromtext': gen_st_linestringfromtext,
+    'st_geomfromwkt': gen_st_geomfromwkt,
+    'st_geomfromtext': gen_st_geomfromtext,
+    'st_astext': gen_st_astext,
 }
 
 if __name__ == "__main__":
